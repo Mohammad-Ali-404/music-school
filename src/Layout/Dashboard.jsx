1922169b5220f71c044ef38cfef48d8d610db2a7
wpmohammad1 <alimohammadjhd@gmail.com>
2023-06-14 17:47:13 +0600

    added something new

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { NavLink, Outlet } from 'react-router-dom';
 import { AiOutlineShoppingCart , AiFillWallet, AiFillHome, AiFillRead } from "react-icons/ai";
-import { BsFillPeopleFill } from "react-icons/bs";
+import { BsBook, BsFillPeopleFill } from "react-icons/bs";
+import { FaUsers } from "react-icons/fa";
 import useCart from '../Hooks/UseCart';
 const Dashboard = () => {
     const [cart] = useCart()
+    const isAdmin = true;
     return (
         <div className="drawer lg:drawer-open">
             <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
@@ -17,15 +19,16 @@ const Dashboard = () => {
             <div className="drawer-side">
                 <label htmlFor="my-drawer-2" className="drawer-overlay"></label> 
                 <ul className="menu p-4 w-80 h-full bg-base-200 text-base-content">
-                {/* Sidebar content here */}
-                <li>
+                {
+                    isAdmin ?<>
+                    <li>
                     <NavLink to='/dashboard/home' style={({ isActive, isPending }) => {
                         return {
                         color: isPending ? "bold" : "",
                         backgroundColor: isActive ? "green" : "",
                         };
                     }}
-                    ><AiFillHome/>User Home</NavLink>
+                    ><AiFillHome/>Admin Home</NavLink>
                 </li>
                 <li>
                     <NavLink to='/dashboard/enrolledclass' style={({ isActive, isPending }) => {
@@ -33,7 +36,7 @@ const Dashboard = () => {
                         color: isPending ? "bold" : "",
                         backgroundColor: isActive ? "green" : "",
                         };
-                    }}><BsFillPeopleFill/>Enrolled Classes</NavLink>
+                    }}><AiFillRead/>ADD Classes</NavLink>
                 </li>
                 <li className='flex'>
                     <NavLink to="/dashboard/mycart"
@@ -43,7 +46,7 @@ const Dashboard = () => {
                         backgroundColor: isActive ? "green" : "",
                         };
                     }}>
-                    <AiOutlineShoppingCart/>My Selected Classes <div className="badge badge-accent">+{cart?.length || 0}</div></NavLink>
+                    <BsBook/>Manage Classes</NavLink>
                     
                 </li>
                 <li>
@@ -52,8 +55,49 @@ const Dashboard = () => {
                         color: isPending ? "bold" : "",
                         backgroundColor: isActive ? "green" : "",
                         };
-                    }}><AiFillWallet/>Payment History</NavLink>
+                    }}><FaUsers/>Manage User</NavLink>
                 </li>
+                    </> :<>
+                    <li>
+                            <NavLink to='/dashboard/home' style={({ isActive, isPending }) => {
+                                return {
+                                color: isPending ? "bold" : "",
+                                backgroundColor: isActive ? "green" : "",
+                                };
+                            }}
+                            ><AiFillHome/>User Home</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to='/dashboard/enrolledclass' style={({ isActive, isPending }) => {
+                                return {
+                                color: isPending ? "bold" : "",
+                                backgroundColor: isActive ? "green" : "",
+                                };
+                            }}><BsFillPeopleFill/>Enrolled Classes</NavLink>
+                        </li>
+                        <li className='flex'>
+                            <NavLink to="/dashboard/mycart"
+                            style={({ isActive, isPending }) => {
+                                return {
+                                color: isPending ? "bold" : "",
+                                backgroundColor: isActive ? "green" : "",
+                                };
+                            }}>
+                            <AiOutlineShoppingCart/>My Selected Classes <div className="badge badge-accent">+{cart?.length || 0}</div></NavLink>
+                            
+                        </li>
+                        <li>
+                            <NavLink to='/dashboard/history' style={({ isActive, isPending }) => {
+                                return {
+                                color: isPending ? "bold" : "",
+                                backgroundColor: isActive ? "green" : "",
+                                };
+                            }}><AiFillWallet/>Payment History</NavLink>
+                        </li>
+                    </>
+                }
+                
+                
                 <div className="divider"></div>
                 <li><NavLink><AiFillHome/>Home</NavLink></li>
                 <li><NavLink><AiFillRead/>Our Classes</NavLink></li>
